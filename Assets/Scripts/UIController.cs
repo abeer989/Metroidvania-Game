b2845f84d6b49b00096089f502e1f33112e706e5
Abeer Mustafa Ahmad <58143445+abeer989@users.noreply.github.com>
2022-06-08 12:47:45 +0500

    Level Designing Started

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -5,7 +5,13 @@ public class UIController : MonoBehaviour
 {
     public static UIController instance;
 
-    public Slider healthBar;
+    [SerializeField] Slider healthBar;
+    [SerializeField] Image fadeScreen;
+    
+    [SerializeField] float fadeSpeed;
+
+    bool fadingToBlack;
+    bool fadingFromBlack;
 
     private void Awake()
     {
@@ -20,9 +26,49 @@ public class UIController : MonoBehaviour
             Destroy(gameObject);
     }
 
+    private void Update()
+    {
+        // if the fadingToBlack is on, the fade image's alpha value will gradually move from 0 -> 1:
+        if (fadingToBlack)
+        {
+            fadeScreen.color = new Color(r: fadeScreen.color.r,
+                                         g: fadeScreen.color.g,
+                                         b: fadeScreen.color.b,
+                                         a: Mathf.MoveTowards(fadeScreen.color.a, 1, fadeSpeed * Time.deltaTime));
+
+            if (fadeScreen.color.a == 1)
+                fadingToBlack = false;
+        }
+
+        // and vice versa if the fadingFromBlack bool is on
+        else if (fadingFromBlack)
+        {
+            fadeScreen.color = new Color(r: fadeScreen.color.r,
+                                         g: fadeScreen.color.g,
+                                         b: fadeScreen.color.b,
+                                         a: Mathf.MoveTowards(fadeScreen.color.a, 0, fadeSpeed * Time.deltaTime));
+
+            if (fadeScreen.color.a == 0)
+                fadingFromBlack = false;
+        }
+    }
+
     public void UpdateHealth(int currentHealth, int maxHealth)
     {
         healthBar.maxValue = maxHealth;
         healthBar.value = currentHealth;
+    }
+
+    // Set bool values via thse functions that will determine the fading in/out:
+    public void SetFadeToBlack()
+    {
+        fadingToBlack = true;
+        fadingFromBlack = false;
+    }
+
+    public void SetFadeFromBlack()
+    {
+        fadingFromBlack = true;
+        fadingToBlack = false;
     }
 }
