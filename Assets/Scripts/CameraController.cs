935290141d71a1c7ad08d01dbccbe11501cc74dc
Abeer Mustafa Ahmad <abeer_hbk@hotmail.com>
2022-08-11 15:50:54 +0500

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -22,9 +22,9 @@ public class CameraController : MonoBehaviour
         // following the player if the player exists within the scene and clamping the cam movement, such that,
         // it never follows the player off of the map:
         if (player)
-            transform.position = new Vector3(Mathf.Clamp(player.position.x, cameraBoundsBox.bounds.min.x + halfWidth, cameraBoundsBox.bounds.max.x - halfWidth),
-                                             Mathf.Clamp(player.position.y, cameraBoundsBox.bounds.min.y + halfHeight, cameraBoundsBox.bounds.max.y - halfHeight),
-                                             transform.position.z);
+            transform.position = new Vector3(x: Mathf.Clamp(player.position.x, cameraBoundsBox.bounds.min.x + halfWidth, cameraBoundsBox.bounds.max.x - halfWidth),
+                                             y: Mathf.Clamp(player.position.y, cameraBoundsBox.bounds.min.y + halfHeight, cameraBoundsBox.bounds.max.y - halfHeight),
+                                             z: transform.position.z);
 
         else
             player = FindObjectOfType<PlayerController>().transform;
